d2912242c97a1c2296d7f9f863e9e7240762dabc
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-27 10:35:05 +0300

    Initial Commit

---
+++ Places App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places App
+//
+//  Created by Eugene Kiselev on 27.07.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
