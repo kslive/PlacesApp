4e41eca762daa7169ee1c5ed060fe802f5ec77f9
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-27 10:36:52 +0300

    Create Table View App.

---
+++ Places App/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  Places App
+//
+//  Created by Eugene Kiselev on 27.07.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
