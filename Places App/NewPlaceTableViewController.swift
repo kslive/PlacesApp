5de81bef90c5b4fc67a480ea4b45ed4c3bddc2e9
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-28 09:21:12 +0300

    Присваивание выбранного изображение пользователем. + UIImagePickerControllerDelegate, UINavigationControllerDelegate.

--- Places App/NewPlaceTableViewController.swift
+++ Places App/NewPlaceTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class NewPlaceTableViewController: UITableViewController {
 
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -50,16 +52,25 @@ extension NewPlaceTableViewController: UITextFieldDelegate {
     }
 }
 
-extension NewPlaceTableViewController {
+extension NewPlaceTableViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func chooseImagePicker(source: UIImagePickerController.SourceType) {
         
         if UIImagePickerController.isSourceTypeAvailable(source) {
             
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = true
             imagePicker.sourceType = source
             present(imagePicker, animated: true)
         }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageOfPlace.image = info[.editedImage] as? UIImage
+        imageOfPlace.contentMode = .scaleAspectFill
+        imageOfPlace.clipsToBounds = true
+        dismiss(animated: true)
     }
 }
