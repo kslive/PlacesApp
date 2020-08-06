35eb1ccf31133aa3d1f5a1871de8496140db12a5
ksllive <zhenya-kiselev-97@mail.ru>
2020-08-06 14:23:48 +0300

    add Map View Controller.

---
+++ Places App/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  Places App
+//
+//  Created by Eugene Kiselev on 06.08.2020.
+//  Copyright © 2020 Eugene Kiselev. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeMapViewController() {
+        
+        dismiss(animated: true)
+    }
+}
